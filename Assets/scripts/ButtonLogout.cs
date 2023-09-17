ad368ca399edf638237f59cfb0d6a4e53fa027fb
SantiagoAndresR <109627166+SantiagoAndresR@users.noreply.github.com>
2023-09-17 14:56:48 -0500

    ss

--- Assets/scripts/ButtonLogout.cs
+++ Assets/scripts/ButtonLogout.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Firebase.Auth;
-using UnityEngine.EventSystems; 
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class ButtonLogout : MonoBehaviour, IPointerClickHandler 
 {
+
+    [SerializeField]
+    private string _SceneToLoad = "Home";
+
     public void OnPointerClick(PointerEventData eventData)
     {
         FirebaseAuth.DefaultInstance.SignOut();
+
+        if (FirebaseAuth.DefaultInstance.CurrentUser == null)
+        {
+            SceneManager.LoadScene(_SceneToLoad);
+        }
     }
 }
