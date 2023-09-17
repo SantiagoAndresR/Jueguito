72e50c27d08414a9d95255b2f352e53323796e86
SantiagoAndresR <109627166+SantiagoAndresR@users.noreply.github.com>
2023-09-17 14:16:04 -0500

    ss

---
+++ Assets/scripts/LoadSceneWhenUserAuthenticated.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Firebase.Auth;
+using System;
+
+public class LoadSceneWhenUserAuthenticated : MonoBehaviour
+{
+
+    [SerializeField]
+    private string _SceneToLoad = "GameScene";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirebaseAuth.DefaultInstance.StateChanged += HandleAuthStateChange;
+    }
+
+    private void HandleAuthStateChange(object sender, EventArgs e)
+    {
+        if(FirebaseAuth.DefaultInstance.CurrentUser != null)
+        {
+            SceneManager.LoadScene(_SceneToLoad);
+        }
+    }
+}
